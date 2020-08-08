6958e8705e3f33c2dff5320ac20691cb951cf564
yafanisonya <yafanison@gmail.com>
2020-08-08 23:02:14 +0800

    achieve router.js

--- src/router.js
+++ src/router.js
@@ -1,12 +1,12 @@
 import Vue from "vue";
 import Router from "vue-router";
-// import NProgress from "nprogress";
+import NProgress from "nprogress";
 import "nprogress/nprogress.css";
-// import NotFound from "./views/404";
+import NotFound from "./views/404";
 
 Vue.use(Router);
 
-export default new Router({
+const router = new Router({
   node: "history",
   base: process.env.BABEL_ENV,
   routes: [
@@ -33,6 +33,105 @@ export default new Router({
         }
       ]
     },
-    {}
+    {
+      path: "/",
+      component: () => {
+        import(/* webpackChunkName: "user" */ "./layouts/BasicLayout");
+      },
+      children: [
+        {
+          path: "/",
+          redirect: "/dashboard/analysis"
+        },
+        {
+          path: "/dashboard",
+          name: "dashboard",
+          component: { render: h => h("router-view") },
+          children: [
+            {
+              path: "/dashboard/analysis",
+              name: "analysis",
+              component: () => {
+                import(
+                  /* webpackChunkName: "dashboard" */ "./views/Dashboard/Analysis"
+                );
+              }
+            }
+          ]
+        },
+        {
+          path: "/form",
+          name: "form",
+          component: { render: h => h("router-view") },
+          children: [
+            {
+              path: "/form/basic-form",
+              name: "basicform",
+              component: () => {
+                import(
+                  /* webpackChunkName: "form" */ "./views/Forms/BasicForm"
+                );
+              }
+            },
+            {
+              path: "/form/step-form",
+              name: "stepform",
+              component: () => {
+                import(/* webpackChunkName: "form" */ "./views/Forms/StepForm");
+              },
+              children: [
+                {
+                  path: "/form/step-form",
+                  redirect: "/form/step-form/info"
+                },
+                {
+                  path: "/form/step-form/info",
+                  name: "info",
+                  component: () => {
+                    import(
+                      /* webpackChunkName: "form" */ "./views/Forms/StepForm/Step1"
+                    );
+                  }
+                },
+                {
+                  path: "/form/step-form/confirm",
+                  name: "confirm",
+                  component: () => {
+                    import(
+                      /* webpackChunkName: "form" */ "./views/Forms/StepForm/Step2"
+                    );
+                  }
+                },
+                {
+                  path: "/form/step-form/result",
+                  name: "result",
+                  component: () => {
+                    import(
+                      /* webpackChunkName: "form" */ "./views/Forms/StepForm/Step3"
+                    );
+                  }
+                }
+              ]
+            }
+          ]
+        }
+      ]
+    },
+    {
+      path: "*",
+      name: "404",
+      component: NotFound
+    }
   ]
 });
+
+router.beforeEach((to, form, next) => {
+  NProgress.start();
+  next();
+});
+
+router.afterEach(() => {
+  NProgress.done();
+});
+
+export default router;
