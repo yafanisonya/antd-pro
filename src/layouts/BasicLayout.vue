1555ba866b03c6b9c1307b68f80ee36f7bcc3b96
yafanisonya <yafanison@gmail.com>
2020-08-09 16:29:06 +0800

    左侧侧边栏

--- src/layouts/BasicLayout.vue
+++ src/layouts/BasicLayout.vue
@@ -1,9 +1,27 @@
 <template>
   <div>
-    <Header />
-    <SiderMenu />
-    <router-view></router-view>
-    <Footer />
+    <a-layout id="components-layout-demo-side" style="min-height: 100vh">
+      <a-layout-sider v-model="collapsed" collapsible :trigger="null">
+        <div class="logo" />
+        <SiderMenu />
+      </a-layout-sider>
+      <a-layout>
+        <a-layout-header style="background: #fff; padding: 0">
+          <a-icon
+            class="trigger"
+            :type="collapsed ? 'menu-unfold' : 'menu-fold'"
+            @click="collapsed = !collapsed"
+          ></a-icon>
+          <Header />
+        </a-layout-header>
+        <a-layout-content style="margin: 0 16px">
+          <router-view></router-view>
+        </a-layout-content>
+        <a-layout-footer style="text-align: center">
+          <Footer />
+        </a-layout-footer>
+      </a-layout>
+    </a-layout>
   </div>
 </template>
 
@@ -12,6 +30,11 @@ import Header from "./Header";
 import SiderMenu from "./SiderMenu";
 import Footer from "./Footer";
 export default {
+  data() {
+    return {
+      collapsed: false
+    };
+  },
   components: {
     Header,
     Footer,
@@ -20,4 +43,13 @@ export default {
 };
 </script>
 
-<style></style>
+<style scoped>
+.trigger {
+  padding: 0 20px;
+  line-height: 64px;
+  font-size: 20px;
+}
+.trigger:hover {
+  background: #eee;
+}
+</style>
--- src/layouts/Footer.vue
+++ src/layouts/Footer.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>Footer</div>
+  <div>Ant Design ©2020 Created by fanison</div>
 </template>
 
 <script>
