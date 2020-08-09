9fe4102e4b33230f4d642808b73c09559bad2750
yafanisonya <yafanison@gmail.com>
2020-08-09 11:42:43 +0800

    router debug

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link to="/dashboard/analysis">dashboard</router-link>
+      <router-link to="/dashboard/analysis">dashboard</router-link>|
       <router-link to="/form">form</router-link>
     </div>
     <router-view />
